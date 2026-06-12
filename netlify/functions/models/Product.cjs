aae30b1828ed485082986896f1fb6acf8da0fbf9
yawnie <eyceyheycey@gmail.com>
2026-06-12 06:02:07 +0300

    Changed products schemas

--- netlify/functions/models/Product.cjs
+++ netlify/functions/models/Product.cjs
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true},
     description: {type: String},
-    price: {type: Number, required: true},
+    price: {type: Number, deafult: 0},
+    isFree: { type: Boolean, default: false},
+    bannerUrl: { type: String},
+    thumbnailUrl: { type: String},
+    previewImages: [{ type: String }],
     category: { type: String},
     fileUrl: { type: String},
-    imageUrl: { type: String},
+    active: {type: Boolean, default: true},
     createdAt: { type: Date, default: Date.now }
 });
 
